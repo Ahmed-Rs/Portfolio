a0815fa6bc69a6675824d4dc3b32aed560ff13a2
Ahmed Rassoul <ahmedrassoulahmed@gmail.com>
2024-03-12 11:19:49 +0100

    Première vue de la plateforme 3d, paramétrage dépendances et fichiers

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,33 @@
-import React from "react";
+/* eslint-disable no-unused-vars */
+import { Canvas } from "@react-three/fiber";
+import React, { Suspense } from "react";
+import Loader from "../components/Loader";
+import Island from "../models/island";
 
 const Home = () => {
   return (
     <section className="w-full h-screen relative">
-      <div className="absolute top-28 left-0 right-0 z-10 flex justify-center items-center">
-        Hi there
-      </div>
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight />
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
+          <Island />
+        </Suspense>
+      </Canvas>
     </section>
   );
 };
 
 export default Home;
+
+{
+  /* <div className="absolute top-28 left-0 right-0 z-10 flex justify-center items-center">
+    Hi there
+  </div> */
+}
