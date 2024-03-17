c135b0f0c6e9b458eab644305b9df48907a38e2e
Ahmed Rassoul <ahmedrassoulahmed@gmail.com>
2024-03-17 08:54:03 +0100

    PopupInfo

--- src/components/PopupInfo.jsx
+++ src/components/PopupInfo.jsx
@@ -1,10 +1,52 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { arrow } from "../assets/icons";
+
+const PopupLink = ({ text, link, btn }) => {
+  return (
+    <div className="text-xl md:text-3xl lg:text-5xl font-bold text-gray-800 drop-shadow-lg p-5 bg-white/70 rounded-lg">
+      <p>{text}</p>
+      <Link to={link} className="flex justify-center items-center gap-3">
+        {btn}
+        <img src={arrow} className="w-4 h-4 object-contain" alt="" />
+      </Link>
+    </div>
+  );
+};
 
 const popupContent = {
-  1: <h1>1</h1>,
-  2: <h1>2</h1>,
-  3: <h1>3</h1>,
-  4: <h1>4</h1>,
+  1: (
+    <PopupLink
+      text={
+        "Salut, je m'appelle Ahmed, je suis développeur web fullstack JS. Je vais te montrer mes projets et mes compétences. Débarque sur l'îlot..."
+      }
+      link={"/about"}
+      btn={"En savoir plus"}
+    />
+  ),
+  2: (
+    <PopupLink
+      text={
+        "Besoin d'un dev ou d'un freelance ? Je suis là pour toi, à quelques cliques."
+      }
+      link={"/contact"}
+      btn={"Contacte moi !"}
+    />
+  ),
+  3: (
+    <PopupLink
+      text={"Ici mon deuxième projet"}
+      link={"/about"}
+      btn={"Visiter le projet"}
+    />
+  ),
+  4: (
+    <PopupLink
+      text={"Ici mon troisième projet"}
+      link={"/about"}
+      btn={"Dive into it !"}
+    />
+  ),
 };
 
 const PopupInfo = ({ currentStage }) => {
--- src/models/island.jsx
+++ src/models/island.jsx
@@ -87,16 +87,16 @@ const Island = ({
       // Set the current stage based on the island's orientation
       switch (true) {
         case normalizedRotation >= 5.45 && normalizedRotation <= 5.85:
-          setCurrentStage(4);
-          break;
-        case normalizedRotation >= 0.85 && normalizedRotation <= 1.3:
-          setCurrentStage(3);
-          break;
-        case normalizedRotation >= 2.4 && normalizedRotation <= 2.6:
-          setCurrentStage(2);
+          setCurrentStage(1);
           break;
         case normalizedRotation >= 4.25 && normalizedRotation <= 4.75:
-          setCurrentStage(1);
+          setCurrentStage(2);
+          break;
+        case normalizedRotation >= 2.4 && normalizedRotation <= 2.6:
+          setCurrentStage(3);
+          break;
+        case normalizedRotation >= 0.85 && normalizedRotation <= 1.3:
+          setCurrentStage(4);
           break;
         default:
           setCurrentStage(null);
